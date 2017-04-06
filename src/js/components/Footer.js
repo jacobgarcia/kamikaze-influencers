dfe8ac841f72f85d19d169b938587c8fc27f8a55
César Guadarrama <cesarguadarrama@icloud.com>
2017-04-06 00:52:38 -0500

    Enhanced footer to match design

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -4,18 +4,20 @@ import { Link } from  'react-router'
 const Footer = (props) => {
   return (
     <div className='footer'>
-      <div className='links'>
-        <ul>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-        <a href='/logout'>Log out</a>
-      </div>
-      <div className='social-links'>
-        <ul>
-          <li><a href=""><img src="/static/img/icons/instagram.png"></img></a></li>
-          <li><a href=""><img src="/static/img/icons/facebook.png"></img></a></li>
-        </ul>
+      <div className='content-section'>
+        <div className='links'>
+          <ul>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+          <a href='/logout'>Log out</a>
+        </div>
+        <div className='social-links'>
+          <ul>
+            <li><a href="https://www.instagram.com/"><img src="/static/img/icons/instagram.png"></img></a></li>
+            <li><a href="https://www.facebook.com/"><img src="/static/img/icons/facebook.png"></img></a></li>
+          </ul>
+        </div>
       </div>
     </div>
   )
