5052d57d7e9790f8a0e3c44da72d61c9b03cd46d
César Guadarrama <cesarguadarrama@icloud.com>
2017-04-06 00:51:53 -0500

    Initial component

---
+++ src/js/components/TimePack.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class TimePack extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+
+    )
+  }
+
+}
+
+export default TimePack
