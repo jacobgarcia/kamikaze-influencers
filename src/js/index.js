f7d642990f3e7fcf8281629f23b291b508351062
César Guadarrama <cesarguadarrama@icloud.com>
2017-04-06 00:56:47 -0500

    Set up window.baseUrl

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom'
 import Routes from './router'
 
+window.baseUrl = 'http://localhost:8080/v1'
+
 const div = document.createElement('div')
 document.body.appendChild(div)
 div.id = 'root'
