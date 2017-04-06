66d9d4eb485b4625c404c66b172f1a8548021ad5
César Guadarrama <cesarguadarrama@icloud.com>
2017-04-06 00:51:14 -0500

    Added getToken function, fixed getProfile and added placeholder for methods to be added

--- src/js/NetworkRequest.js
+++ src/js/NetworkRequest.js
@@ -1,20 +1,86 @@
 import axios from 'axios'
 
 const token = window.localStorage.getItem('token')
-const baseUrl = `http://localhost:8080/v1`
+
+// Authorization header interceptor
+axios.interceptors.request.use((config) => {
+  // Get last url route and check if it's different from authenticate to add header
+  if (config.url.split('/').pop() !== 'authenticate')
+    config.headers.Authorization = `Bearer ${token}`
+
+  return config
+}, (error) => {
+    return Promise.reject(error)
+})
 
 class NetworkRequest {
   constructor() {
 
   }
 
-  static getProfile(callback, errCallback) {
-    axios(`${baseUrl}/users/self?access_token=${token}`)
+  static setUser(callback, errCallback) {
+    axios.post(`${window.baseUrl}/users`)
     .then((res) => {
       callback(res)
     })
-    .catch((err) => {
-      errCallback(err)
+    .catch((error) => {
+      errCallback(error)
+    })
+  }
+
+  static setLiking() {
+
+  }
+
+  static disableLiking() {
+
+  }
+
+  static setCommenting(comment) {
+
+  }
+
+  static disableCommenting() {
+
+  }
+
+  static setFollowing() {
+
+  }
+
+  static disableFollowing() {
+
+  }
+
+  static getHallOfFame() {
+
+  }
+
+  static setPayment() {
+
+  }
+
+  static setTime() {
+
+  }
+
+  static getToken(code, callback, errCallback) {
+    axios.post(`${window.baseUrl}/users/authenticate`, { code })
+    .then((res) => {
+      callback(res)
+    })
+    .catch((error) => {
+      errCallback(error)
+    })
+  }
+
+  static getProfile(callback, errCallback) {
+    axios(`${window.baseUrl}/users/self`)
+    .then((res) => {
+      callback(res)
+    })
+    .catch((error) => {
+      errCallback(error)
     })
   }
 
