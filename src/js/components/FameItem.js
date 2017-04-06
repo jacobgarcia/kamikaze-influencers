02f228274f5b46b2b55c0b1053c5a9dea53deaa1
César Guadarrama <cesarguadarrama@icloud.com>
2017-04-06 00:52:15 -0500

    Improved component

--- src/js/components/FameItem.js
+++ src/js/components/FameItem.js
@@ -18,7 +18,8 @@ class FameItem extends Component {
         <div className='fame-item-content'>
           <div className='profile'>
             <div className='profile-picture'><img src={user.profile_picture}></img></div>
-            <span>@{user.username}</span>
+            <span>{user.username}</span>
+            <span>{user.fullName}</span>
           </div>
           <input type="button" value="+"></input>
         </div>
