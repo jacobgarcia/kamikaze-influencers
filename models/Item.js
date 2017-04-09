17019f4308432524c7ae51e114d655639065b6b9
César Guadarrama <cesarguadarrama@icloud.com>
2017-04-09 16:32:31 -0500

    Created schema v1.0

---
+++ models/Item.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const schema = new Schema({
+  name: String,
+  description: String,
+  days: { type: Number, required: true },
+  price: { type: Number, required: true },
+  discount: Number,
+  type: { type: Number, default: 0 }
+})
+
+module.exports = mongoose.model('Item', schema)
