9a4de6522b4dcf11265969f6e615dc8e7a1531cc
César Guadarrama <cesarguadarrama@icloud.com>
2017-04-09 16:32:15 -0500

    Created schema v1.0

--- models/Payment.js
+++ models/Payment.js
@@ -2,11 +2,21 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const schema = new Schema({
-  paypal: {
-    id: { type: String, required: true },
-    date: { type: String, required: true },
-    amount: Number
-  }
+  paypal_id: { type: String, required: true, unique: true },
+  item_id: { type: Schema.Types.ObjectId, ref: 'Item', required: true },
+  amount: Number,
+  payer: {
+    payment_method: String,
+    payer_info: {
+      email: String,
+      first_name: String,
+      last_name: String,
+      payer_id: String,
+      country_code: String
+    }
+  },
+  username: { type: String, ref: 'User', required: true },
+  date: { type: Date, default: Date.now }
 })
 
-module.exports = schema
+module.exports = mongoose.model('Payment', schema)
