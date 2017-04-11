2f26aa1f31d9d1a58fcecbe8eb7f0288067a1f28
César Guadarrama <cesarguadarrama@icloud.com>
2017-04-10 23:02:21 -0500

    Changes for production landing

--- src/js/router.js
+++ src/js/router.js
@@ -3,29 +3,11 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 import Landing from './containers/Landing'
 import App from './containers/App'
-import Dashboard from './containers/Dashboard'
-import Profile from './containers/Profile'
-import Authenticate from './containers/Authenticate'
-import Logout from './containers/Logout'
-import Time from './containers/Time'
-
-const isAuthenticated = () => {
-  const token = localStorage.getItem('token')
-
-  if (token) {
-    return true
-  }
-  return false
-}
 
 const Routes = (
   <Router history={browserHistory}>
     <Route path='/' component={App}>
-      <IndexRoute component={ isAuthenticated() ? Dashboard : Landing }/>
-      <Route path='profile' component={Profile}/>
-      <Route path='authenticate' component={Authenticate}/>
-      <Route path='logout' component={Logout}/>
-      <Route path='time' component={Time}/>
+      <IndexRoute component={ Landing }/>
     </Route>
 
   </Router>
