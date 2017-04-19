93c4a36399ff906c42b5007b068439d4fa5ae5ca
César Guadarrama <cesarguadarrama@icloud.com>
2017-04-19 13:59:32 -0500

    Config for better debugging

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,66 @@
 const gulp = require('gulp')
+const sourcemaps = require('gulp-sourcemaps')
 const sass = require('gulp-sass')
 const nodemon = require('gulp-nodemon')
 const rename = require('gulp-rename')
 const webpackStream = require('webpack-stream')
 const webpack = require('webpack')
-const webpackConfig = require('./config/webpack.config.js')
-const webpackDevConfig = require('./config/dev.webpack.config.js')
 const path = require('path')
-const Server = require('karma').Server
+const webpackConfig = require(path.resolve('config/webpack.config.js'))
+const webpackConfigDev = require(path.resolve('config/webpack-dev.config.js'))
 
+// Possible duplicate?
 gulp.task('sass', () =>
-  gulp.src('./src/styles/master.scss')
-  .pipe(sass({ outputStyle: 'compressed' })
-  .on('error', sass.logError))
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(gulp.dest('./dist'))
+  gulp.src(path.resolve('src/styles/master.scss'))
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('./dist/styles'))
 )
 
-gulp.task('devwebpack', () =>
-  gulp.src(path.resolve('src/js/index.js'))
-  .pipe(webpackStream(webpackDevConfig, webpack))
-  .on('error', function handleError() {
-    this.emit('end')
-  })
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(gulp.dest('./dist'))
+// Possible duplicate?
+gulp.task('sassDev', () =>
+  gulp.src(path.resolve('src/styles/master.scss'))
+      .pipe(sourcemaps.init())
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(sourcemaps.write())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('./dist/styles'))
 )
 
+// Possible duplicate?
 gulp.task('webpack', () =>
   gulp.src(path.resolve('src/js/index.js'))
-  .pipe(webpackStream(webpackConfig, webpack))
-  .on('error', function handleError() {
-    this.emit('end')
-  })
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(gulp.dest('./dist'))
+      .pipe(webpackStream(webpackConfig, webpack)) // Create sourcemaps for better debbuging
+      .on('error', function handleError() {
+        this.emit('end')
+      })
+      .pipe(gulp.dest('./dist'))
 )
 
+// Possible duplicate?
+gulp.task('webpackDev', () =>
+  gulp.src(path.resolve('src/js/index.js'))
+      .pipe(sourcemaps.init())
+      .pipe(webpackStream(webpackConfigDev, webpack)) // Create sourcemaps for better debbuging
+      .on('error', function handleError() {
+        this.emit('end')
+      })
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('./dist'))
+)
 
 gulp.task('start', () => {
-    nodemon({
-        script: 'server.js',
-        ignore: ['/src'],
-        env: {
-            'NODE_ENV': 'development'
-        }
-    })
-})
-
-gulp.task('test', (done) => {
-  new Server({
-    configFile: path.resolve('karma.conf.js'),
-    singleRun: true
-  }, done).start()
+  nodemon({
+    script: 'server.js',
+    ignore: ['/src/**', '/dist'],
+    env: { 'NODE_ENV': 'development' }
+  })
 })
 
 gulp.task('watch', () => {
-    gulp.watch(['src/js/**/*.js', 'src/js/**/*.vue'], ['devwebpack'])
-    gulp.watch('src/styles/**.scss', ['sass'])
+  gulp.watch('src/js/**/*.js', ['webpackDev'])
+  gulp.watch('src/styles/**.scss', ['sassDev'])
 })
 
-gulp.task('develop', ['sass', 'devwebpack', 'start', 'watch'])
+gulp.task('develop', ['sassDev', 'webpackDev', 'start', 'watch'])
 gulp.task('build', ['sass', 'webpack'])
