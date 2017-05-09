f897436d97394e8757fc618ac382309007c3e88a
Jacob Garcia <rock2494@hotmail.com>
2017-05-09 13:50:19 -0500

    Updated Footer

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -9,13 +9,13 @@ const Footer = (props) => {
       <div className='content-section'>
         <div className='links'>
           <ul>
-            <li><a href='/about'>{Localization.about}</a></li>
-            <li><a href='/contact'>{Localization.contact}</a></li>
+            <a href='mailto:hola@owainfluencers.com'><li>{Localization.contact}</a></li>
             <li><a href='/terms'>{Localization.usage}</a></li>
           </ul>
           {props.loggedin ? <a href='/logout'>{Localization.logout}</a> : ''}
         </div>
         <div className='social-links'>
+          <span>Copyright © OWA Influencers</span>
           <ul>
             <li><Link href="https://www.instagram.com/owainfluencersco/"><img src="/static/img/icons/instagram.png"></img></Link></li>
             <li><Link href="https://www.facebook.com/owainfluencers/"><img src="/static/img/icons/facebook.png"></img></Link></li>
