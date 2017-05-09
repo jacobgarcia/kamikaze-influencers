63e417af420ddf8406741e85be43be9163100dde
Jacob Garcia <rock2494@hotmail.com>
2017-05-09 14:07:26 -0500

    Focus on mount for search bar now disabled

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -9,17 +9,17 @@ const Footer = (props) => {
       <div className='content-section'>
         <div className='links'>
           <ul>
-            <a href='mailto:hola@owainfluencers.com'><li>{Localization.contact}</a></li>
+            <li><a href='mailto:hola@owainfluencers.com'>{Localization.contact}</a></li>
             <li><a href='/terms'>{Localization.usage}</a></li>
           </ul>
           {props.loggedin ? <a href='/logout'>{Localization.logout}</a> : ''}
         </div>
         <div className='social-links'>
-          <span>Copyright © OWA Influencers</span>
           <ul>
             <li><Link href="https://www.instagram.com/owainfluencersco/"><img src="/static/img/icons/instagram.png"></img></Link></li>
             <li><Link href="https://www.facebook.com/owainfluencers/"><img src="/static/img/icons/facebook.png"></img></Link></li>
           </ul>
+          <span>Copyright © OWA Influencers</span>
         </div>
       </div>
     </div>
